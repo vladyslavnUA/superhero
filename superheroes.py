7167a8e008bc0ea403cb94ec93df62b4587896c2
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-04 16:51:25 -0800

    added hero class

--- superheroes.py
+++ superheroes.py
@@ -1,5 +1,13 @@
 import random
 
+class Hero:
+    def __init__(self, name, starting_health=100):
+        self.abilities = []
+        self.armors = []
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+
 class Ability:
     def __init__(self, name, max_damage):
         self.name = name
@@ -15,7 +23,15 @@ class Armor:
         self.name = name
         self.max_block = max_block
 
+    def block(self):
+        random_block = random.randint(0, self.max_block)
+        return random_block #self.max_block
+
 if __name__ == "__main__":
     ability = Ability("Debugging Ability", 20)
     print(ability.name)
     print(ability.attack())
+
+    armor = Armor("Debugging Shield", 10)
+    print(armor.name)
+    print(armor.block())
